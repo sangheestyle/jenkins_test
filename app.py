c01dc2ccf4e7a7fa031ae8c609fc9dea988922fa
Sanghee Kim <sangheestyle@gmail.com>
2015-06-23 02:12:10 -0600

    wrong imports

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys, os
+
 print("Hello world!")
 print("Hello world 2!")
 print("Hello world 3!")
@@ -6,6 +8,5 @@ print("Hello world 5!")
 print("Hello world 6!")
 print("Hello world 7!")
 for i in range(0, 10):
-  # violate pep8
-  print i
-  print i
+    print i
+    print i
